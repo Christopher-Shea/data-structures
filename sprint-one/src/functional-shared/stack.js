36010331cdb126c98b92c6bafab1e9d305239f55
Christopher-Shea <ctshea1@gmail.com>
2018-09-19 16:07:44 -0500

    "Implimented code for functional-shared Stack"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.index = 0;
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.index++;
+  this.storage[this.index] = value;
+};
+
+stackMethods.pop = function() {
+  let popped = this.storage[this.index];
+  delete this.storage[this.index];
+  this.index = Math.max(this.index - 1, 0);
+  return popped;
+};
+
+stackMethods.size = function() {
+  return this.index;
+};
+
 
 
