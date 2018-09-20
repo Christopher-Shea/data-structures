0184ca6d17607dfcb9d0d4ce13a36fa397b5196d
Christopher-Shea <ctshea1@gmail.com>
2018-09-20 11:28:44 -0500

    implement singly-linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,43 @@
-var LinkedList = function() {
-  var list = {};
+const LinkedList = function() {
+  let list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    let newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+    }
+    if (list.tail) {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head) {
+      let popped = list.head.value;
+      list.head = (list.head.next) ? list.head.next : null;
+      return popped;
+    }
   };
 
   list.contains = function(target) {
+    let checkNext = function(nextNode) {
+      if (nextNode.value === target) {
+        return true;
+      }
+      if (nextNode.next) {
+        return checkNext(nextNode.next)
+      }
+      return false;
+    }
+    return checkNext(list.head);
+
+    //let currentNode = (list.head) ? list.head : list; 
+    // if (currentNode.next) {
+    //   return list.contains.call(currentNode.next, target);
+    // }
   };
 
   return list;
@@ -26,4 +54,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
  */
