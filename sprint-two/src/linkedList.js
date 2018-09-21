fd4b76845176690a1158b7151feadda1b3127978
Christopher-Shea <ctshea1@gmail.com>
2018-09-20 20:11:04 -0500

    added functionality to singly-linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,6 +3,17 @@ const LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  list.addToHead = function(value) {
+    let newNode = Node(value);
+    if (!list.tail) {
+      list.tail = newNode;
+    }
+    if (list.head) {
+      newNode.next = list.head;
+    }
+    list.head = newNode;
+  };
+
   list.addToTail = function(value) {
     let newNode = Node(value);
     if (!list.head) {
@@ -22,6 +33,19 @@ const LinkedList = function() {
     }
   };
 
+  list.insert = function(parentValue, value) {
+    let newNode = Node(value);
+    let parentNode = list.head;
+    while (parentNode.value !== parentValue) {
+      if (parentNode.next === null) {
+        return null;
+      }
+      parentNode = parentNode.next;
+    }
+    newNode.next = parentNode.next;
+    parentNode.next = newNode;
+  };
+
   list.contains = function(target) {
     // MOST STRAIGHTFORWARD - WHILE LOOP
     let currentNode = list.head;
@@ -33,19 +57,6 @@ const LinkedList = function() {
     }
     return false;
   };
-
-  // USING A HELPER FUNCTION
-  // let checkNext = function(nextNode) {
-  //   if (nextNode.value === target) {
-  //     return true;
-  //   }
-  //   if (nextNode.next) {
-  //     return checkNext(nextNode.next)
-  //   }
-  //   return false;
-  // }
-  // return checkNext(list.head);
-  //};
 
   return list;
 };
@@ -67,9 +78,23 @@ var Node = function(value) {
 */
 
 /*
- Wishlist: 
- addToHead
- removeFromTail
- Insert
- double-linked
+Wishlist:
+test suite:
+  addtoHead: check that after adding node to empty list, head and tail values are the same, and head and tail nexts are null
+             check that when adding to head to a lsit with an existing node, head points to new node and new node next points to old head next 
+  insert: 
 */
+
+// USING A HELPER FUNCTION FOR CONTAINS
+  // list.contains = function(target) {
+  //   let checkNext = function(nextNode) {
+  //     if (nextNode.value === target) {
+  //       return true;
+  //     }
+  //     if (nextNode.next) {
+  //       return checkNext(nextNode.next)
+  //     }
+  //     return false;
+  //   }
+  //   return checkNext(list.head);
+  // };
